7231b8b692a963b62e1e83efcb7695a475b297bc
Ahmed Ramadan <ahmedramadan8050@gmail.com>
2023-08-13 18:27:36 +0300

    beforefinal

---
+++ app/src/main/java/com/example/cinemaapp/FavViewModel.kt
@@ -0,0 +1,39 @@
+package com.example.cinemaapp
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cinemaapp.model.Movie
+import com.example.cinemaapp.model.MovieDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class FavViewModel:ViewModel() {
+    val favLiveData = MutableLiveData<List<Movie>>()
+    val insertLiveData = MutableLiveData<Boolean>()
+
+
+
+    fun viewfavMovie(dao: MovieDao?) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val viewMovie = dao?.readAllData()
+            withContext(Dispatchers.Main) {
+                viewMovie?.let {
+                    favLiveData.value = it
+                }            }
+        }
+
+    }
+    fun insertMovie(dao: MovieDao?, movie: Movie) {
+        viewModelScope.launch(Dispatchers.IO) {
+            dao?.addMovie(movie)
+            withContext(Dispatchers.Main) {
+                insertLiveData.value = true
+            }
+
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/cinemaapp/model/Movie.kt
@@ -0,0 +1,12 @@
+package com.example.cinemaapp.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+@Entity(tableName = "Movie_Table")
+data class Movie(
+    @PrimaryKey
+var posterPath : String,
+var title:String,
+var overView:String
+)
